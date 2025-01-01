a8c25bd6de8245f33da6114037ac3741913b1837
AdityaL <94298390+Josh101101@users.noreply.github.com>
2025-01-01 11:45:30 +0530

    Add files via upload
    Initial Commit

---
+++ home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Gurgaon Real Estate Analytics App",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Housing Buddy! 👋")
+
+st.write("This app provides insights into the real estate market.")
+st.write("Our intuitive tool helps you estimate property values in Gurgaon. Explore interactive data visualizations, including a map and a word cloud of key features, to gain deeper insights into the local market.")
+
+    
